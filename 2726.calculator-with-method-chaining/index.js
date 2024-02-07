57784d085c56cf2aa166a08a27e258fd564a7bb4
Skuddin <sheikh@nextgenitltd.com>
2024-02-07 17:11:32 +0600

    Calculator with Method Chaining

---
+++ 2726.calculator-with-method-chaining/index.js
@@ -0,0 +1,42 @@
+class Calculator {
+    
+  
+  constructor(value) {
+      this.result = value;
+  }
+ 
+  add(value){
+    this.result += value;
+    return this;
+  }
+  
+ 
+  subtract(value){
+    this.result -= value;
+    return this;
+  }
+  
+ 
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+  
+ 
+  divide(value) {
+    this.result /= value;
+    return this;
+  }
+ 
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+  
+  getResult() {
+      return this.result;
+  }
+}
+
+const calculator = new Calculator(10).add(12).subtract(2);
+console.log(calculator);
