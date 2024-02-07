0082f495bb695dedfa3f97f5746ae2ff89773592
Skuddin <sheikh@nextgenitltd.com>
2024-02-07 16:33:00 +0600

    array wrapper class

--- 2695. array-wrapper/index.js
+++ 2695. array-wrapper/index.js
@@ -1,1 +1,26 @@
-console.log('hlw');
+class ArrayWrapper{
+  constructor(nums){
+    this.nums = nums;
+    
+  }
+
+  valueOf(){
+    return this.sum = nums.reduce((prev,curr) => {
+      return prev + curr;
+    });
+  }
+
+  toString(){
+    return `[${this.nums.join(',')}]`
+  }
+
+    
+};
+
+// const nums = [[1,2],[3,4]];
+// const operation = "Add";
+const obj1 = new ArrayWrapper([23,98,42,70]);
+// const obj2 = new ArrayWrapper([3,4]);
+// const sum = obj1 + obj2;
+const sum = String(obj1);
+console.log(sum);
